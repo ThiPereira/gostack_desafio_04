c46b27953cd3ceeb07d65108b24dfdf95600d678
ThiPereira <thiago_frank@yahoo.com.br>
2020-05-07 20:01:23 -0300

    final

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:3333",
+  //baseURL: "http://localhost",
+  //baseURL: "10.0.2.2",
+  //baseURL: "10.0.2.2:3333",
 });
 
 export default api;
